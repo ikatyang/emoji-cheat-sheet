91caa1b3875f9d8c9b6d023eaa76d9957d894574
ikatyang <ikatyang@gmail.com>
2017-07-16 00:19:14 +0800

    chore(travis): update cron script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+yarn run generate
+
+git config --global user.name ikatyang-bot
+git config --global user.email ikatyang+bot@gmail.com
+git config --global push.default simple
+
+git add --all
+git commit -m "docs(readme): update emoji-cheat-sheet"
+git push -q
